59a0efde9c4977926f3c2768b13f4bbc81d09d47
Your Name <you@example.com>
2023-02-10 02:07:19 +0400

    second commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,13 @@ let drone = document.querySelector(".Drone");
 let dronediv = document.querySelector(".dronediv");
 let accesories = document.querySelector(".accesories");
 let accesoriesdiv = document.querySelector(".accesoriesdiv");
-
+let hamburgerMenu = document.getElementById("hamburgermenu");
+let hamburgermenuDiv = document.querySelector(".hamburgerMenuDiv");
+let closeButton = document.getElementById("closebutton");
+let personMenu = document.getElementById("personMenu");
+let personDiv = document.querySelector(".personDiv");
+let searchMenu = document.getElementById("searchMenu");
+let searchDiv = document.querySelector(".searchDiv");
 
 drone.addEventListener("mouseover", function () {
   dronediv.classList.add("active");
@@ -12,13 +18,23 @@ drone.addEventListener("mouseout", function () {
   dronediv.classList.remove("active");
 });
 
-
-
 accesories.addEventListener("mouseover", function () {
-    accesoriesdiv.classList.add("active");
+  accesoriesdiv.classList.add("active");
 });
 
-
 accesories.addEventListener("mouseout", function () {
-    accesoriesdiv.classList.remove("active");
+  accesoriesdiv.classList.remove("active");
+});
+
+hamburgerMenu.addEventListener("click", function () {
+  hamburgermenuDiv.classList.toggle("active");
+});
+closeButton.addEventListener("click", function () {
+  hamburgermenuDiv.classList.remove("active");
+});
+personMenu.addEventListener("click", function () {
+  personDiv.classList.toggle("active");
+});
+searchMenu.addEventListener("click", function () {
+  searchDiv.classList.toggle("active");
 });
