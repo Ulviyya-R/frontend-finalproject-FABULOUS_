7b21b4213ac1fcdd5cb73ace830b527ddaa29a83
Your Name <you@example.com>
2023-02-11 20:01:10 +0400

    5 commit

--- assets/js/HTCtouch.js
+++ assets/js/HTCtouch.js
@@ -36,4 +36,30 @@ $(".responsive").slick({
   ],
 });
 
+// $(document).ready(function () {
+//   $(".set > a").on("click", function () {
+//     if ($(this).hasClass("active")) {
+//       $(this).removeClass("active");
+//       $(this).siblings(".content").slideUp(200);
+//       $(".set > a i").removeClass("fa-minus").addClass("fa-plus");
+//     } else {
+//       $(".set > a i").removeClass("fa-minus").addClass("fa-plus");
+//       $(this).find("i").removeClass("fa-plus").addClass("fa-minus");
+//       $(".set > a").removeClass("active");
+//       $(this).addClass("active");
+//       $(".content").slideUp(200);
+//       $(this).siblings(".content").slideDown(200);
+//     }
+//   });
+// });
 
+$(document).ready(function () {
+  $("#description").click(function () {
+    $("#textcontent").slideToggle("slow");
+    $("#formcontent").slideUp("slow");
+  });
+  $("#reviews").click(function () {
+    $("#formcontent").slideToggle("slow");
+    $("#textcontent").slideUp("slow");
+  });
+});
