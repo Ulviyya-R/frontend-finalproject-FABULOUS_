5b7546c4b2ba59c6614aa57165579063b82afee4
Your Name <you@example.com>
2023-02-12 17:59:44 +0400

    6 commit

--- assets/js/HTCtouch.js
+++ assets/js/HTCtouch.js
@@ -63,3 +63,43 @@ $(document).ready(function () {
     $("#textcontent").slideUp("slow");
   });
 });
+
+
+
+$(".responsive").slick({
+  dots: false,
+  infinite: false,
+  variableWidth: true,
+  speed: 300,
+  autoplay: false,
+  autoplaySpeed: 2000,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  prevArrow: '<i class="fa-solid fa-chevron-left left_arrow">',
+  nextArrow: '<i class="fa-solid fa-chevron-right right_arrow">',
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: false,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
