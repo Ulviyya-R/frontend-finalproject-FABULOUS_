29619355309ff6dffdd569564cfabc097d3e6400
Your Name <you@example.com>
2023-02-15 02:17:47 +0400

    create basket menu and ..

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,11 @@
+//#region Header
 let drone = document.querySelector(".Drone");
 let dronediv = document.querySelector(".dronediv");
 let accesories = document.querySelector(".accesories");
 let accesoriesdiv = document.querySelector(".accesoriesdiv");
+let basket = document.querySelector("#basketIcon");
+let basketMenu = document.querySelector(".basketMenuDiv");
+let closeBasketButton = document.querySelector("#closebasket-button");
 let hamburgerMenu = document.getElementById("hamburgermenu");
 let hamburgermenuDiv = document.querySelector(".hamburgerMenuDiv");
 let closeButton = document.getElementById("closebutton");
@@ -17,8 +21,18 @@ drone.addEventListener("mouseover", function () {
 
 drone.addEventListener("mouseout", function () {
   dronediv.classList.remove("active");
-
 });
+
+basket.addEventListener("click",function(){
+  basketMenu.classList.toggle("active");
+  
+})
+// document.body.addEventListener("click",function(){
+//   basketMenu.classList.remove("active");
+// })
+closeBasketButton.addEventListener("click",function(){
+  basketMenu.classList.remove("active");
+})
 
 accesories.addEventListener("mouseover", function () {
   accesoriesdiv.classList.add("active");
@@ -41,28 +55,31 @@ personMenu.addEventListener("click", function () {
 searchMenu.addEventListener("click", function () {
   searchDiv.classList.toggle("active");
 });
+//#endregion
 
-
-$('#customer-review .owl-carousel').owlCarousel({
+//#region CUSTOMER-REVIEW SECTION START
+$("#customer-review .owl-carousel").owlCarousel({
   stagePadding: 50,
-  loop:false,
-  dots:false,
- 
-  margin:10,
-  nav:true,
-  responsive:{
-      0:{
-          items:1
-      },
-      621:{
-          items:2
-      },
-      1000:{
-          items:2
-      }
-  }
-})
+  loop: false,
+  dots: false,
 
+  margin: 10,
+  nav: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    621: {
+      items: 2,
+    },
+    1000: {
+      items: 2,
+    },
+  },
+});
+//#endregion
+
+//#region  FAQ SECTION START
 $(".accordion-header").click(function () {
   let $icon = $(this).find("i");
   $icon.toggleClass("active");
@@ -70,22 +87,68 @@ $(".accordion-header").click(function () {
   $(".accordion-body").not($(this).next()).slideUp();
   $(".accordion-header i").not($icon).removeClass("active");
 });
+//#endregion
 
-
-$('#sponsors  .owl-carousel').owlCarousel({
-  loop:true,
-  margin:10,
+//#region SPONSORS SECTION START
+$("#sponsors  .owl-carousel").owlCarousel({
+  loop: true,
+  margin: 10,
   autoplay: true,
   autoplayTimeout: 2500,
-  responsive:{
-      0:{
-          items:1
-      },
-      600:{
-          items:1
-      },
-      1000:{
-          items:1
-      }
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 1,
+    },
+    1000: {
+      items: 1,
+    },
+  },
+});
+//#endregion
+
+//#region ALL-PRODUCTS SECTION START
+$("#All-Products .owl-carousel").owlCarousel({
+  stagePadding: 20,
+  loop: false,
+  dots: false,
+  margin: 10,
+  nav: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    621: {
+      items: 2,
+    },
+    1000: {
+      items: 3,
+    },
+  },
+});
+
+var activeCat = "";
+function filterGroup(group) {
+  if (activeCat != group) {
+    $(".view")
+      .filter("." + group)
+      .show();
+    $(".view")
+      .filter(":not(." + group + ")")
+      .hide();
+    activeCat = group;
   }
-})
+}
+
+$(".featured-button").click(function () {
+  filterGroup("featured-carusel");
+});
+$(".bestSellers-button").click(function () {
+  filterGroup("bestSellers-carusel ");
+});
+$(".newArrivals-button").click(function () {
+  filterGroup("newArrivals-carusel ");
+});
+//#endregion
