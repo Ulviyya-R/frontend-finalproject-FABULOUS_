e01e4d5a6ffb1c30e7fbc9fe9a730fcd94c756ce
Your Name <you@example.com>
2023-02-15 19:45:45 +0400

    changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,16 +23,14 @@ drone.addEventListener("mouseout", function () {
   dronediv.classList.remove("active");
 });
 
-basket.addEventListener("click",function(){
-  basketMenu.classList.toggle("active");
-  
-})
-// document.body.addEventListener("click",function(){
-//   basketMenu.classList.remove("active");
-// })
-closeBasketButton.addEventListener("click",function(){
+basket.addEventListener("click", function () {
+  basketMenu.classList.add("active");
+});
+closeBasketButton.addEventListener("click", function (e) {
+  e.stopPropagation();
   basketMenu.classList.remove("active");
-})
+  document.body.style.overflowY ="scroll";
+});
 
 accesories.addEventListener("mouseover", function () {
   accesoriesdiv.classList.add("active");
@@ -55,6 +53,135 @@ personMenu.addEventListener("click", function () {
 searchMenu.addEventListener("click", function () {
   searchDiv.classList.toggle("active");
 });
+//#endregion
+
+//#region Basket
+
+let basketIconbut = document.getElementById("basketIcon");
+let basketHTML = document.querySelector(".basketMenuDiv");
+let itemul = document.querySelector(".item");
+
+document.addEventListener("DOMContentLoaded", function () {
+  let basketStr = localStorage.getItem("basket");
+  let basket = JSON.parse(basketStr);
+  if (!basket || !basket.length) {
+    localStorage.setItem("basket", JSON.stringify([]));
+  } else {
+    ShowCount(basket);
+    ShowTotalPrice(basket);
+
+  }
+});
+basketIconbut.addEventListener("click", function () {
+  // // basketHTML.classList.add("active");
+
+  document.body.style.overflow = "hidden";
+
+  let basket = JSON.parse(localStorage.getItem("basket"));
+
+  itemul.innerHTML = " ";
+
+  basket.forEach((item) => {
+    let html = `
+    <li class ="d-flex mb-3">
+   <div class="image">
+     <img src="${item.src}" alt="">
+   </div>
+   <div class="info">
+   <span>${item.count}</span>
+   x
+    <h6>
+    ${item.name}
+    </h6>
+    <span>
+    ${item.price}
+    </span>
+    
+    </div>
+    <div class="xbtn">
+    <b style ="color:black">
+    <i class="fa-regular fa-trash-can"></i>
+    </b>
+    </div>
+    
+    </li>
+    `;
+    itemul.innerHTML += html;
+  });
+
+  let delbutton = document.querySelectorAll(".xbtn");
+  delbutton.forEach((del) => {
+    del.addEventListener("click", function () {
+      let li = this.parentElement;
+      let name = li.querySelector(".info  h6").innerText;
+      basket = basket.filter((drone) => drone.name != name);
+      li.remove();
+
+      ShowCount(basket);
+    ShowTotalPrice(basket);
+
+      localStorage.setItem("basket", JSON.stringify(basket));
+    });
+  });
+});
+
+let addtocartButton = document.querySelectorAll(".hover-button-addcart");
+addtocartButton.forEach((button) => {
+  button.addEventListener("click", function () {
+    // basketHTML.classList.toggle("active");
+
+    let basket = JSON.parse(localStorage.getItem("basket"));
+    if (!basket) {
+      localStorage.setItem("basket", JSON.stringify([]));
+      basket = JSON.parse(localStorage.getItem("basket"));
+    }
+    let product = GetProductsData(this);
+    let existedProduct = basket.find((pro) => {
+      return pro.id == product.id;
+    });
+    if (!existedProduct) {
+      basket.push(product);
+    } else {
+      existedProduct.count++;
+    }
+    ShowCount(basket);
+    ShowTotalPrice(basket);
+    // console.log(basket);
+    let basketStr = JSON.stringify(basket);
+    localStorage.setItem("basket", basketStr);
+
+
+  
+});
+  });
+
+  
+
+function GetProductsData(product) {
+  let parent = product.parentElement.parentElement;
+  let id = parent.getAttribute("data-id");
+  let price = parent.querySelector(".pricess").innerText;
+  let name = parent.querySelector(".product-name").innerText;
+  let src = parent.querySelector("img").src;
+  let result = { id, price, name, src, count: 1 };
+  return result;
+}
+function ShowCount(basket) {
+  let basketCount = document.querySelector(".basket-count");
+  basketCount.innerText = basket.reduce((total, product) => {
+    return (total += product.count);
+  }, 0);
+}
+
+function ShowTotalPrice(basket) {
+  let total = document.querySelector(".totalprice");
+
+  total.innerText = basket.reduce((total, product) => {
+    return (total += parseInt(product.price) * product.count);
+  }, 0);
+}
+
+
 //#endregion
 
 //#region CUSTOMER-REVIEW SECTION START
